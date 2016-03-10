38dc1024718e328d25c76bf80ecab34f02c0a038
Laith - Mac Pro <macpro@Siberias-Mac-Pro.local>
2016-03-09 17:05:46 -0800

    revert

--- serverSetup.js
+++ serverSetup.js
@@ -15,7 +15,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 module.exports = function( )
 {
 	var promise = new mongoose.Promise( );
-	console.log( 'test' );
 
 
 	// Grab all the models.
